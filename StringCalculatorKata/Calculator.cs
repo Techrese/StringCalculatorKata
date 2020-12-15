90e6f68b418cee61e2bc87d389b109f2176abbd6
tim clinckemalie <tim.clinckemalie@hotmail.com>
2020-12-15 16:06:53 +0100

    Add project files.

---
+++ StringCalculatorKata/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StringCalculatorKata
+{
+    internal class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        internal int Add(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StringCalculatorKata/CalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit;
+using NUnit.Framework;
+
+namespace StringCalculatorKata
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator calculator = new Calculator();
+
+        [Test]
+        public void WhenGivenEmptyString_ReturnZero()
+        {
+            string input = string.Empty;
+
+            int result = calculator.Add(input);
+            Assert.AreEqual(0,result);
+        }
+    }
+}
