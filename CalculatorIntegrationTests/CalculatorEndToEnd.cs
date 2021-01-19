5fa8de844a4746e4e0ca64c09920802ad937f51a
Aoriseth <aoriseth@gmail.com>
2021-01-19 18:54:54 +0100

    Added integration tests

---
+++ CalculatorIntegrationTests/CalculatorEndToEnd.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace CalculatorIntegrationTests
+{
+    public class CalculatorEndToEnd
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
